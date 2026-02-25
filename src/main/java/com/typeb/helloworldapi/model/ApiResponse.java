20d156cafd3160cbf27fc55726ed3c4b1a4187ed
ashenkleo <ashenkleo@gmail.com>
2026-02-25 10:22:32 +0530

    feat: implement the endpoint and the logic for validating name

---
+++ src/main/java/com/typeb/helloworldapi/model/ApiResponse.java
@@ -0,0 +1,8 @@
+package com.typeb.helloworldapi.model;
+
+/**
+ * Keeping the class empty because the API is returning a simple JSON map at the moment
+ * Implement this if a custom response format is required
+ */
+public class ApiResponse {
+}
